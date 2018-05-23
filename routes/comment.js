31b668a6e6101d545702e43f34b6b3804aa4fff2
changxing1999 <you@example.com>
2018-05-23 11:52:07 +0800

    1

--- routes/comment.js
+++ routes/comment.js
@@ -3,8 +3,20 @@ var router = express.Router();
 var sql=require('../libs/sql')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
- 
+router.get('/', function(req, res) {
+    sql("select * from comment",function (err,data) {
+         res.send(data);
+    })
+});
+router.get('/del', function(req, res) {
+    let id=req.query.id
+    sql(`delete from comment where id=${id}`,function (err,data) {
+        if(data.affectedRows){
+            res.send("ok");
+        }else{
+            res.send("on");
+        }
+    })
 });
 
 module.exports = router;
