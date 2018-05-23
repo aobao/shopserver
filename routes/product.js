29a19e97c534c65881a1e69d45b46af15eb0e9e5
wangshijie <1571026979@qq.com>
2018-05-23 18:35:42 +0800

    王
    王

--- routes/product.js
+++ routes/product.js
@@ -1,26 +1,44 @@
 var express = require('express');
 var router = express.Router();
 var sql=require('../libs/sql')
-
+var multer=require('multer')
+var os=require('os')
+var fs=require('fs')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    sql('select * from product',function(err,data){
+    sql(`select * from product `,function(err,data){
         res.send(data);
     })
 });
+var upload=multer({dest:os.tmpdir()})
+router.post('/upload',upload.single('file'),function(req,res,next){
+    res.send(req.file.path);
+})
 router.post('/add',function(req,res){
    let data=req.body,
        title=data.title,
        name=data.name,
        con=data.con,
        econ=data.econ,
-       img=data.img,
        offprice=data.offprice,
        price=data.price,
        amount=data.amount,
        degra=data.degra,
        sap=data.sap,
        type=data.type;
+  let  img=JSON.parse(body.img);
+  console.log(img);
+  let str=[];
+  img.forEach(val=>{
+      if(val.response){
+          let newpath='/'+Date.now()+val.name;
+          fs.renameSync(val.response,'./public'+newpath);
+          str.push({name:val.name,url:newpath})
+      }else{
+          str.push({name:val.name,url:val.url});
+      }
+  })
+  img=JSON.stringify(str);    
     sql(`insert into product(title,name,con,econ,img,offprice,price,amount,degra,sap,type) 
     values('${title}','${name}','${con}','${econ}','${img}','${offprice}','${price}','${amount}','${degra}','${sap}','${type}')`,function(err,mes){
         if(err) throw err;
@@ -40,13 +58,24 @@ router.post('/edit',function(req,res){
         name=data.name,
         con=data.con,
         econ=data.econ,
-        img=data.img,
         offprice=data.offprice,
         price=data.price,
         amount=data.amount,
         degra=data.degra,
         sap=data.sap,
         type=data.type;
+  let  img=JSON.parse(body.img);
+  let str=[];
+  img.forEach(val=>{
+      if(val.response){
+          let newpath='/'+Date.now()+val.name;
+          fs.renameSync(val.response,'./public'+newpath);
+          str.push({name:val.name,url:newpath})
+      }else{
+          str.push({name:val.name,url:val.url});
+      }
+  })
+  img=JSON.stringify(str);   
      sql(`update product set title='${title}',name='${name}',con='${con}',econ='${econ}',img='${img}',offprice='${offprice}',price=${price},amount='${amount}',degra='${degra}',sap='${sap}',type='${type}' where id=${id}`,function(err,mes){
          if(err) throw err;
          if(mes.affectedRows){
