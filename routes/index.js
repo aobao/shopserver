e592b6fcffafb23e6b1f4982463b4c404736e250
changxing1999 <you@example.com>
2018-06-01 16:06:50 +0800

    c indexlogin

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,37 @@ var sql=require('../libs/sql')
 
 /* GET home page. */
 
-router.post('/login',function(req,res) {
-    console.log(1);
-    res.send("ok")
-});
-router.post('/isg', function(req,res) {
-    res.send("ok")
-});
 router.get('/', function(req,res) {
     res.send("ok,/")
 });
+router.post('/login',function(req,res) {
+   let user=req.body.user
+   let pass=req.body.pass
+    sql(`select * from user where name='${user}'`,function (err,data) {
+        if(data.length){
+            if(pass==data[0].pass){
+                res.send("ok");
+            }else{
+                res.send("no");
+            }
+        }else{
+            res.send("no");
+            return;
+        }
+    })
+});
+router.post('/isg', function(req,res) {
+    let user=req.body.user
+    let pass=req.body.pass
+    sql(`insert into user (name,pass) values ("${user}","${pass}")`,function (err,data) {
+        if(data.affectedRows){
+            res.send("ok")
+            return
+        }else{
+            res.send("no")
+            return
+        }
+    })
+});
+
 module.exports = router;
